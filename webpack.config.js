e6d5ce89129ab5026c4b84409580f1dcd7c0bf77
diehardkamikaze <diehardkamikaze@gmail.com>
2021-03-28 16:36:28 +0300

    my first webpack config

--- webpack.config.js
+++ webpack.config.js
@@ -1,135 +1,27 @@
 const path = require('path');
-const HTMLWebpackPlugin = require('html-webpack-plugin');
-const { CleanWebpackPlugin } = require('clean-webpack-plugin');
-const CopyWebpackPlugin = require('copy-webpack-plugin');
-const MiniExtractCssPlugin = require('mini-css-extract-plugin');
-const TerserWebpackPlugin = require('terser-webpack-plugin');
-const OptimizeCssAssetsWebpackPlugin = require('optimize-css-assets-webpack-plugin');
-//const { BundleAnalyzerPlugin } = require('webpack-bundle-analyzer');
-
-const isDev = process.env.NODE_ENV === 'development';
-const isProd = !isDev;
-
-const pluginsL = () => {
-  const base = [
-    new HTMLWebpackPlugin({
-      template: './index.html',
-      minify: {
-        collapseWhitespace: isProd,
-      },
-    }),
-    new CleanWebpackPlugin(),
-    new CopyWebpackPlugin({
-      patterns: [{
-        from: path.resolve(__dirname, 'src/favicon.png'),
-        to: path.resolve(__dirname, 'dist'),
-      }],
-    }),
-    new MiniExtractCssPlugin({ filename: filename2('.css') }),
-  ];
-  /*if (isProd)
-  {
-    base.push(new BundleAnalyzerPlugin());
-  }*/
-  return base;
-};
-
-const jsLoaders = () => {
-  const config = [{
-    loader: 'babel-loader',
-    options: {
-      presets: ['@babel/preset-env'],
-      plugins: ['@babel/plugin-proposal-class-properties'],
-    },
-  }];
-  if (isDev) {
-   // config.push('eslint-loader');
-  }
-  return (config);
-};
-
-const optimization = () => {
-  const config = {
-    splitChunks: {
-      chunks: 'all',
-    },
-  };
-  if (isProd) {
-    config.minimizer = [
-      new OptimizeCssAssetsWebpackPlugin(),
-      new TerserWebpackPlugin()];
-  }
-  return config;
-};
-
-function filename2(ext) {
-  if (isDev) {
-    return `[name]${ext}`;
-  }
-  return `[name].[contenthash]${ext}`;
-}
+const ESLintPlugin = require('eslint-webpack-plugin');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const CopyPlugin = require('copy-webpack-plugin');
 
 module.exports = {
-  context: path.resolve(__dirname, 'src'),
-  entry: {
-    main: ['@babel/polyfill', './index.js'],
-    analytics: './analytics.ts',
-  },
+  entry: path.resolve(__dirname, 'src/index.js'),
   output: {
-    filename: filename2('.js'),
     path: path.resolve(__dirname, 'dist'),
+    filename: '[name].[contenthash].js',
+    clean: true,
   },
-  devtool: isDev ? 'source-map' : false,
-  plugins: pluginsL(),
-  optimization: optimization(),
+  plugins: [new ESLintPlugin({
+    extensions: ['js'],
+  }), new HtmlWebpackPlugin({
+    template: path.resolve(__dirname, 'src/index.html'),
+  }), new CopyPlugin({
+    patterns: [{
+      from: path.resolve(__dirname, 'src/images'), to: path.resolve(__dirname, 'dist'),
+    },
+    ],
+  })],
   devServer: {
-    port: 4200,
-    hot: isProd,
-  },
-  resolve: {
-    extensions: ['.js', '.json'],
-    alias: {
-      '@models': path.resolve(__dirname, 'src/models'),
-      '@': path.resolve(__dirname, 'src'),
-    },
-  },
-  module: {
-    rules: [{
-      test: /\.css$/,
-      use: [MiniExtractCssPlugin.loader, 'css-loader'],
-    },
-    {
-      test: /\.less$/,
-      use: [MiniExtractCssPlugin.loader, 'css-loader', 'less-loader'],
-    },
-    {
-      test: /\.(png|jpg|svg|gif)$/,
-      use: ['file-loader'],
-    }, {
-      test: /\.(ttf|eot|woff|woff2)$/,
-      use: ['file-loader'],
-    }, {
-      test: /\.xml$/,
-      use: ['xml-loader'],
-    }, {
-      test: /\.csv$/,
-      use: ['csv-loader'],
-    },
-    {
-      test: /\.js$/,
-      exclude: '/node_modules/',
-      use: jsLoaders(),
-    },
-    {
-      test: /\.ts$/,
-      exclude: '/node_modules/',
-      use: {
-        loader: 'babel-loader',
-        options: {
-          presets: ['@babel/preset-env', '@babel/preset-typescript'],
-          plugins: ['@babel/plugin-proposal-class-properties'],
-        },
-      },
-    }],
+    port: 1480,
+    contentBase: path.resolve(__dirname, 'dist'),
   },
 };
