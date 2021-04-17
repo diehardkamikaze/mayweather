693dfcb11fd6ceebbb8c8efb635be65d69c56c3b
diehardkamikaze <diehardkamikaze@gmail.com>
2021-04-17 21:32:29 +0300

    fixed bg image downloading and loader

--- src/buttons_handlers/buttons_handler.js
+++ src/buttons_handlers/buttons_handler.js
@@ -1,14 +1,51 @@
 import addLoader from '../loader/loader';
+import getJSON from '../utils/getjson';
+import rusToLat from '../utils/translit';
 
-const buttonHandler = (elem, logic) => {
-  const clickedButton = window.masterObj.buttons[elem];
-  const elementLoader = addLoader(window.masterObj[elem]);
+const changeBgLogic = () => new Promise(((resolve) => {
+  getJSON('https://api.unsplash.com/photos/random?orientation=landscape&per_page=1&query=nature&client_id=e2077ad31a806c894c460aec8f81bc2af4d09c4f8104ae3177bb809faf0eac17')
+    .then((result) => { window.masterObj.elements.bg.src = result.urls.regular; resolve(1); });
+}));
+
+const buttonBgHandler = (event) => {
+  const clickedButton = event.target;
+  const elementLoader = addLoader(document.body);
   clickedButton.disabled = true;
-  logic(elem)
+  window.masterObj.elements.bg.onload = () => {
+    document.body.removeChild(elementLoader);
+  };
+  changeBgLogic()
     .then(() => {
       clickedButton.disabled = false;
-      window.masterObj[elem].removeChild(elementLoader);
     });
 };
+/*
+const changeLangLogic
 
-export default buttonHandler;
+const buttonLangHandler
+
+const changeUnitLogic
+
+const buttonUnitHandler
+*/
+const changeSearchLogic = (location) => new Promise(((resolve) => {
+  getJSON(`https://api.weatherapi.com/v1/forecast.json?key=363474e96d194f10ab9212718201105&q=${location}&days=3`)
+    .then((result) => { console.log(result); resolve(1); });
+}));
+
+const buttonSearchHandler = (event) => {
+  if (!window.masterObj.elements.search.value) return alert('Введите значение');
+  let location = '';
+  const clickedButton = event.target;
+  const elementLoader = addLoader(window.masterObj.elements.map);
+  clickedButton.disabled = true;
+  location = rusToLat(window.masterObj.elements.search.value);
+  changeSearchLogic(location)
+    .then(() => {
+      clickedButton.disabled = false;
+    });
+  // где-то позже закрой лоадер
+  return 'something';
+};
+
+export { changeBgLogic, buttonBgHandler, buttonSearchHandler };
--- src/index.js
+++ src/index.js
@@ -1,9 +1,8 @@
 import './styles.css';
 import './assets/fonts/Roboto/Roboto-Regular.ttf';
 import addLoader from './loader/loader';
-import getJSON from './api/getjson';
-import { changeBgLogic } from './buttons_handlers/buttons_logic';
-import buttonHandler from './buttons_handlers/buttons_handler';
+import getJSON from './utils/getjson';
+import { changeBgLogic, buttonBgHandler, buttonSearchHandler } from './buttons_handlers/buttons_handler';
 
 const zZ = addLoader(document.body);
 window.masterObj = {
@@ -11,20 +10,54 @@ window.masterObj = {
 };
 
 function initAll() {
-  changeBgLogic('bg');
-  window.masterObj.bg = document.body;
+  if (!localStorage.getItem('userParams')) localStorage.setItem('userParams', { lang: 'ru', unit: 'c' });
 
+  window.masterObj.lang = {};
+  window.masterObj.lang.city = {
+    obj: document.querySelector('.city'),
+    en: '',
+    ru: '',
+  };
+  window.masterObj.lang.country = {
+    obj: document.querySelector('.country'),
+    en: '',
+    ru: '',
+  };
 
+  /* interface elements */
+  window.masterObj.elements = {};
+  window.masterObj.elements.bg = document.querySelector('.background');
+  window.masterObj.elements.map = document.querySelector('.location-info');
+  window.masterObj.elements.forecastToday = document.querySelector('.forecast-for-today');
+  window.masterObj.elements.forecastTheeDays = document.querySelector('.thee-days-forecast');
+
+  /* control block elements */
   window.masterObj.buttons = {};
   window.masterObj.buttons.bg = document.querySelector('.change-bg');
   window.masterObj.buttons.unit = document.querySelector('.change-unit');
   window.masterObj.buttons.lang = document.querySelector('.change-lang');
+  window.masterObj.buttons.search = document.querySelector('.search-go');
+
+  window.masterObj.elements.search = document.querySelector('#search-input');
+
   /* add event listeners */
-  window.masterObj.buttons.bg.addEventListener('click', () => buttonHandler('bg', changeBgLogic));
+  window.masterObj.buttons.bg.addEventListener('click', buttonBgHandler);
+  window.masterObj.buttons.search.addEventListener('click', buttonSearchHandler);
+  /* init bg */
+  changeBgLogic();
+
+  /* get location and weatherForecast  */
+  getJSON('http://ipinfo.io/?token=8f1769cb9f55d9')
+    .then((userLocation) => {
+      window.masterObj.lang.city.en = userLocation.city;
+      window.masterObj.lang.country.en = userLocation.country;
+      return getJSON(`https://api.weatherapi.com/v1/forecast.json?key=363474e96d194f10ab9212718201105&q=${userLocation.city}&days=3`);
+    }).then((weatherForecast) => {
+      console.log(weatherForecast);
+    });
+  /* get map */
 }
 
 initAll();
 
 setTimeout(() => { document.body.removeChild(zZ); }, 1000);
-
-getJSON('http://ipinfo.io/?token=8f1769cb9f55d9').then((result) => { console.log(result); });
