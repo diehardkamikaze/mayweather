9978add1e16216b543c1424c1b07fae4df194922
diehardkamikaze <diehardkamikaze@gmail.com>
2021-04-19 01:42:45 +0300

    full logic

--- src/buttons_handlers/buttons_handler.js
+++ src/buttons_handlers/buttons_handler.js
@@ -1,11 +1,12 @@
 import addLoader from '../loader/loader';
 import getJSON from '../utils/getjson';
 import rusToLat from '../utils/translit';
+import errorHandler from '../utils/errorhandler';
+import { fillUnit } from '../utils/changeunit';
+import { fillWeatherData, fillMeta, fillWithImages } from '../utils/changelang';
 
-const changeBgLogic = () => new Promise(((resolve) => {
-  getJSON('https://api.unsplash.com/photos/random?orientation=landscape&per_page=1&query=nature&client_id=e2077ad31a806c894c460aec8f81bc2af4d09c4f8104ae3177bb809faf0eac17')
-    .then((result) => { window.masterObj.elements.bg.src = result.urls.regular; resolve(1); });
-}));
+const changeBgLogic = () => getJSON('https://api.unsplash.com/photos/random?orientation=landscape&per_page=1&query=nature&client_id=e2077ad31a806c894c460aec8f81bc2af4d09c4f8104ae3177bb809faf0eac17')
+  .then((result) => { window.masterObj.elements.bg.src = result.urls.regular; });
 
 const buttonBgHandler = (event) => {
   const clickedButton = event.target;
@@ -19,33 +20,111 @@ const buttonBgHandler = (event) => {
       clickedButton.disabled = false;
     });
 };
-/*
-const changeLangLogic
 
-const buttonLangHandler
+const changeLangLogic = (language) => {
+  const keys = Object.keys(window.masterObj.lang);
+  keys.forEach((key) => {
+    window.masterObj.lang[key].element.innerHTML = window.masterObj.lang[key][language];
+  });
+  // костыль
+  window.masterObj.elements.search.placeholder = window.masterObj.lang.searchinpt.element.innerHTML;
+};
 
-const changeUnitLogic
+const buttonLangHandler = (event) => {
+  setTimeout(() => {
+    localStorage.setItem('userLang', event.target.innerHTML);
+    const clickedButton = event.target;
+    clickedButton.disabled = true;
+    changeLangLogic(event.target.innerHTML.toLowerCase());
+    clickedButton.disabled = false;
+  }, 0);
+};
 
-const buttonUnitHandler
-*/
-const changeSearchLogic = (location) => new Promise(((resolve) => {
-  getJSON(`https://api.weatherapi.com/v1/forecast.json?key=363474e96d194f10ab9212718201105&q=${location}&days=3`)
-    .then((result) => { console.log(result); resolve(1); });
-}));
+const changeUnitLogic = (unit) => {
+  const keys = Object.keys(window.masterObj.unit);
+  keys.forEach((key) => {
+    window.masterObj.unit[key].element.innerHTML = window.masterObj.unit[key][unit];
+  });
+};
+
+const buttonUnitHandler = (event) => {
+  setTimeout(() => {
+    localStorage.setItem('userUnit', event.target.innerHTML);
+    const clickedButton = event.target;
+    clickedButton.disabled = true;
+    changeUnitLogic(event.target.innerHTML);
+    clickedButton.disabled = false;
+  }, 0);
+};
+
+const locationLogic = (location) => {
+  window.masterObj.elements.maplon.innerHTML = `${location.lon}° / ${location.lon * 60}′ E`;
+  window.masterObj.elements.maplat.innerHTML = `${location.lat}° / ${location.lat * 60}′ N`;
+  window.masterObj.elements.mapframe.src = `https://api.mapbox.com/styles/v1/mapbox/dark-v10.html?title=true&zoomwheel=false&access_token=pk.eyJ1IjoiYWxtYXpvbi05NSIsImEiOiJja25tdnF4YzEwd2FqMm9wZTVkYW9kMThyIn0.3TivI8Cmoa_f22ocNJDVwg#9/${location.lat}/${location.lon}`;
+};
+
+const changeSearchLogic = (location, lang) => getJSON(`https://api.weatherapi.com/v1/forecast.json?key=363474e96d194f10ab9212718201105&q=${encodeURIComponent(location)}&days=3&lang=${encodeURIComponent(lang)}`)
+  .then((result) => result,
+    () => getJSON(`https://api.weatherapi.com/v1/forecast.json?key=363474e96d194f10ab9212718201105&q=${encodeURIComponent(rusToLat(location))}&days=3&lang=${encodeURIComponent(lang)}`).then((result) => result, errorHandler));
 
 const buttonSearchHandler = (event) => {
   if (!window.masterObj.elements.search.value) return alert('Введите значение');
-  let location = '';
+  let locationName = '';
   const clickedButton = event.target;
-  const elementLoader = addLoader(window.masterObj.elements.map);
+  const mapLoader = addLoader(window.masterObj.elements.map);
+  window.masterObj.elements.mapframe.src = ''; // не догнал зачем так делать
+  window.masterObj.elements.mapframe.onload = () => {
+    window.masterObj.elements.map.removeChild(mapLoader);
+  };
+  const dayLoader = addLoader(window.masterObj.elements.forecastToday);
+  const days3Loader = addLoader(window.masterObj.elements.forecastTheeDays);
   clickedButton.disabled = true;
-  location = rusToLat(window.masterObj.elements.search.value);
-  changeSearchLogic(location)
+  locationName = window.masterObj.elements.search.value;
+  let weatherTmp = '';
+  changeSearchLogic(locationName, 'ru')
+    .then((weatherForecast) => {
+      weatherTmp = weatherForecast;
+      locationLogic(weatherForecast.location);
+      fillUnit(weatherForecast);
+      fillWeatherData(weatherForecast, 'ru');
+      return changeSearchLogic(locationName, 'en');
+    }, errorHandler)
+    .then((weatherForecast) => {
+      fillWeatherData(weatherForecast, 'en');
+      return weatherForecast.location;
+    })
+    .then((location) => {
+      const promises = [
+        getJSON(`https://api.opencagedata.com/geocode/v1/json?key=ce72918744584fb49873a292493d3550&q=${encodeURIComponent(`${location.lat},${location.lon}`)}&pretty=1&no_annotations=1&language=en`)
+          .then((result) => { fillMeta(result, 'en'); }, errorHandler),
+        getJSON(`https://api.opencagedata.com/geocode/v1/json?key=ce72918744584fb49873a292493d3550&q=${encodeURIComponent(`${location.lat},${location.lon}`)}&pretty=1&no_annotations=1&language=ru`)
+          .then((result) => { fillMeta(result, 'ru'); }, errorHandler)];
+      return Promise.allSettled(promises);
+    }, errorHandler)
     .then(() => {
+      fillWithImages([
+        weatherTmp.current.condition.icon,
+        weatherTmp.forecast.forecastday[0].day.condition.icon,
+        weatherTmp.forecast.forecastday[1].day.condition.icon,
+        weatherTmp.forecast.forecastday[2].day.condition.icon,
+      ], () => {
+        window.masterObj.elements.forecastToday.removeChild(dayLoader);
+        window.masterObj.elements.forecastTheeDays.removeChild(days3Loader);
+        changeUnitLogic(localStorage.getItem('userUnit'));
+        changeLangLogic(localStorage.getItem('userLang').toLowerCase());
+      });
       clickedButton.disabled = false;
     });
-  // где-то позже закрой лоадер
   return 'something';
 };
 
-export { changeBgLogic, buttonBgHandler, buttonSearchHandler };
+export {
+  changeBgLogic,
+  buttonBgHandler,
+  buttonSearchHandler,
+  changeUnitLogic,
+  buttonUnitHandler,
+  changeLangLogic,
+  buttonLangHandler,
+  locationLogic,
+};
--- src/index.js
+++ src/index.js
@@ -2,34 +2,49 @@ import './styles.css';
 import './assets/fonts/Roboto/Roboto-Regular.ttf';
 import addLoader from './loader/loader';
 import getJSON from './utils/getjson';
-import { changeBgLogic, buttonBgHandler, buttonSearchHandler } from './buttons_handlers/buttons_handler';
+import {
+  getTranslateObj, fillWeatherData,
+  fillMeta, fillWithImages,
+} from './utils/changelang';
+import { getUnitObj, fillUnit } from './utils/changeunit';
+import {
+  changeBgLogic, buttonBgHandler, buttonSearchHandler,
+  changeUnitLogic, buttonUnitHandler, changeLangLogic,
+  buttonLangHandler, locationLogic,
+} from './buttons_handlers/buttons_handler';
+import errorHandler from './utils/errorhandler';
 
-const zZ = addLoader(document.body);
-window.masterObj = {
-  bg_img: '',
-};
+const pageLoading = addLoader(document.body);
+window.masterObj = {};
 
 function initAll() {
-  if (!localStorage.getItem('userParams')) localStorage.setItem('userParams', { lang: 'ru', unit: 'c' });
+  /* close general loader logic */
+  let loaderCounter = 0;
+  function dataDone() {
+    if (loaderCounter > 2) {
+      document.body.removeChild(pageLoading);
+    }
+    loaderCounter += 1;
+    changeUnitLogic(localStorage.getItem('userUnit'));
+    changeLangLogic(localStorage.getItem('userLang').toLowerCase());
+  }
 
-  window.masterObj.lang = {};
-  window.masterObj.lang.city = {
-    obj: document.querySelector('.city'),
-    en: '',
-    ru: '',
-  };
-  window.masterObj.lang.country = {
-    obj: document.querySelector('.country'),
-    en: '',
-    ru: '',
-  };
+  /* clock timer */
+  setInterval(() => {
+    window.masterObj.elements.time.innerHTML = new Date().toLocaleTimeString();
+  }, 1000);
 
   /* interface elements */
   window.masterObj.elements = {};
   window.masterObj.elements.bg = document.querySelector('.background');
   window.masterObj.elements.map = document.querySelector('.location-info');
+  window.masterObj.elements.mapframe = document.querySelector('.location-info iframe');
+  window.masterObj.elements.maplon = document.querySelector('.longitude span');
+  window.masterObj.elements.maplat = document.querySelector('.latitude span');
   window.masterObj.elements.forecastToday = document.querySelector('.forecast-for-today');
-  window.masterObj.elements.forecastTheeDays = document.querySelector('.thee-days-forecast');
+  window.masterObj.elements.forecastTheeDays = document.querySelector('.three-days-forecast');
+
+  window.masterObj.elements.time = document.querySelector('.time');
 
   /* control block elements */
   window.masterObj.buttons = {};
@@ -40,24 +55,50 @@ function initAll() {
 
   window.masterObj.elements.search = document.querySelector('#search-input');
 
+  if (!localStorage.getItem('userLang')) localStorage.setItem('userLang', 'ru');
+  if (!localStorage.getItem('userUnit')) localStorage.setItem('userUnit', 'C');
+
+  window.masterObj.lang = getTranslateObj();
+  window.masterObj.unit = getUnitObj();
+
   /* add event listeners */
   window.masterObj.buttons.bg.addEventListener('click', buttonBgHandler);
   window.masterObj.buttons.search.addEventListener('click', buttonSearchHandler);
-  /* init bg */
-  changeBgLogic();
+  window.masterObj.buttons.unit.addEventListener('click', buttonUnitHandler);
+  window.masterObj.buttons.lang.addEventListener('click', buttonLangHandler);
 
-  /* get location and weatherForecast  */
+  /* init bg */
+  changeBgLogic().then(dataDone);
+
+  /* get location, then weatherForecast, then   */
   getJSON('http://ipinfo.io/?token=8f1769cb9f55d9')
-    .then((userLocation) => {
-      window.masterObj.lang.city.en = userLocation.city;
-      window.masterObj.lang.country.en = userLocation.country;
-      return getJSON(`https://api.weatherapi.com/v1/forecast.json?key=363474e96d194f10ab9212718201105&q=${userLocation.city}&days=3`);
-    }).then((weatherForecast) => {
+    .then((userLocation) => getJSON(`https://api.weatherapi.com/v1/forecast.json?key=363474e96d194f10ab9212718201105&q=${encodeURIComponent(userLocation.city)}&days=3&lang=ru`),
+      errorHandler)
+    .then((weatherForecast) => {
       console.log(weatherForecast);
+      locationLogic(weatherForecast.location);
+      fillUnit(weatherForecast);
+      fillWeatherData(weatherForecast, 'ru');
+      fillWithImages([
+        weatherForecast.current.condition.icon,
+        weatherForecast.forecast.forecastday[0].day.condition.icon,
+        weatherForecast.forecast.forecastday[1].day.condition.icon,
+        weatherForecast.forecast.forecastday[2].day.condition.icon,
+      ], dataDone);
+      return getJSON(`https://api.weatherapi.com/v1/forecast.json?key=363474e96d194f10ab9212718201105&q=${encodeURIComponent(weatherForecast.location.name)}&days=3&lang=en`);
+    })
+    .then((weatherForecast) => {
+      fillWeatherData(weatherForecast, 'en');
+      return weatherForecast.location;
+    }, errorHandler)
+    .then((location) => {
+      getJSON(`https://api.opencagedata.com/geocode/v1/json?key=ce72918744584fb49873a292493d3550&q=${encodeURIComponent(`${location.lat},${location.lon}`)}&pretty=1&no_annotations=1&language=en`)
+        .then((result) => { fillMeta(result, 'en'); dataDone(); }, errorHandler);
+      getJSON(`https://api.opencagedata.com/geocode/v1/json?key=ce72918744584fb49873a292493d3550&q=${encodeURIComponent(`${location.lat},${location.lon}`)}&pretty=1&no_annotations=1&language=ru`)
+        .then((result) => { fillMeta(result, 'ru'); dataDone(); }, errorHandler);
     });
-  /* get map */
 }
 
-initAll();
-
-setTimeout(() => { document.body.removeChild(zZ); }, 1000);
+window.masterObj = {
+};
+document.addEventListener('DOMContentLoaded', initAll);
